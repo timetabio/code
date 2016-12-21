8e649bd950ab962beaba6c51cf877d5f6a45a2a3
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-12-21 17:48:08 +0100

    Add folds to container builds

--- scripts/build-containers.sh
+++ scripts/build-containers.sh
@@ -8,6 +8,14 @@ log () {
   tput sgr0
 }
 
+fold_start () {
+  echo -en "travis_fold:start:${1}\r"
+}
+
+fold_end () {
+  echo -en "travis_fold:end:${1}\r"
+}
+
 if [ -z ${VERSION} ]; then
   echo "Refusing to build without VERSION. Stop"
   exit
@@ -15,22 +23,32 @@ fi
 
 set -e
 
+fold_start "Worker"
 log "Building worker"
 docker build -t docker.ttio.cloud:5000/web/worker:latest -f "${BASE_DIR}/containers/ttio-worker/Dockerfile" ${BASE_DIR}
 docker tag docker.ttio.cloud:5000/web/worker:latest docker.ttio.cloud:5000/web/worker:${VERSION}
+fold_end "Worker"
 
+fold_start "API"
 log "Building api"
 docker build -t docker.ttio.cloud:5000/web/api:latest -f "${BASE_DIR}/containers/ttio-api/Dockerfile" ${BASE_DIR}
 docker tag docker.ttio.cloud:5000/web/api:latest docker.ttio.cloud:5000/web/api:${VERSION}
+fold_end
 
+fold_start "Frontend"
 log "Building frontend"
 docker build --build-arg VERSION=${VERSION} -t docker.ttio.cloud:5000/web/frontend:latest -f "${BASE_DIR}/containers/ttio-frontend/Dockerfile" ${BASE_DIR}
 docker tag docker.ttio.cloud:5000/web/frontend:latest docker.ttio.cloud:5000/web/frontend:${VERSION}
+fold_end "Frontend"
 
+fold_start "Survey"
 log "Building survey"
 docker build --build-arg VERSION=${VERSION} -t docker.ttio.cloud:5000/web/survey:latest -f "${BASE_DIR}/containers/ttio-survey/Dockerfile" ${BASE_DIR}
 docker tag docker.ttio.cloud:5000/web/survey:latest docker.ttio.cloud:5000/web/survey:${VERSION}
+fold_end "Survey"
 
+fold_start "Proxy"
 log "Building proxy"
 docker build --build-arg VERSION=${VERSION} -t docker.ttio.cloud:5000/web/proxy:latest -f "${BASE_DIR}/containers/ttio-proxy/Dockerfile" ${BASE_DIR}
 docker tag docker.ttio.cloud:5000/web/proxy:latest docker.ttio.cloud:5000/web/proxy:${VERSION}
+fold_end "Proxy"
