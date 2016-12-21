6844bfb9b115a670d08218f1a716632a0fd21558
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-12-21 16:23:39 +0100

    Update travis config

---
+++ .travis/setup.sh
@@ -0,0 +1,4 @@
+ssh-agent -s
+openssl aes-256-cbc -K $encrypted_f4692d717c58_key -iv $encrypted_f4692d717c58_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
+chmod 600 .travis/deploy_key.pem
+ssh-add .travis/deploy_key.pem
