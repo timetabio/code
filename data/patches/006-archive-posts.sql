3fd6d656d9b940285327c2124d1f59db79238810
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-12-29 20:17:46 +0100

    Re-create aggregated_posts

--- data/patches/006-archive-posts.sql
+++ data/patches/006-archive-posts.sql
@@ -10,3 +10,13 @@ CREATE VIEW expired_archived_posts AS
   SELECT * FROM posts
   WHERE archived IS NOT NULL
     AND archived < (utc_now() - INTERVAL '30 days');
+
+DROP VIEW aggregated_posts;
+CREATE VIEW aggregated_posts AS
+  SELECT posts.*,
+    users.username as author_username,
+    users.name as author_name,
+    feeds.name as feed_name
+  FROM posts
+    JOIN users ON posts.author_id = users.id
+    JOIN feeds ON posts.feed_id = feeds.id;
