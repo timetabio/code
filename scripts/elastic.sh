d39da06573b2a10b568260c2b89e09e54d00da95
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-12-31 18:25:43 +0100

    Update elastic.sh, remove duplicate logic in dev.sh

--- scripts/elastic.sh
+++ scripts/elastic.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
 
 ROOT=$(cd $(dirname $0)/../ && pwd)
-RESET=0
-BOOTSTRAP=0
-REINDEX=0
 
-for ARG in $@; do
-  case ${ARG} in
-    --reset)
-      RESET=1
-      BOOTSTRAP=1
-      REINDEX=1
-      ;;
-    --bootstrap)
-      BOOTSTRAP=1
-      ;;
-    --reindex)
-      REINDEX=1
-      ;;
-    *)
-      echo "Unknown flag ${ARG}"
-      exit 1
-  esac
-done
-
-if [ -z ${1} ]; then
-  echo "Usage: ${0} [--reset] [--bootstrap] [--reindex]"
-  exit 1
-fi
-
-if [ ${RESET} -eq 1 ]; then
+reset () {
   docker run --rm \
       --net ttio-dev-net \
       docker.ttio.cloud:5000/library/elastic \
       sh -c 'curl -s -X DELETE http://ttio-elastic:9200/ttio'
   echo ""
   sleep 2
-fi
+  mappings
+  index
+}
 
-if [ ${BOOTSTRAP} -eq 1 ]; then
+mappings () {
   docker run --rm \
     --net ttio-dev-net \
     -v ${ROOT}/data/elastic-mappings.json:/mappings.json \
     docker.ttio.cloud:5000/library/elastic \
     sh -c 'curl -s -X PUT http://ttio-elastic:9200/ttio -d @/mappings.json'
   echo ""
+}
+
+index () {
+  ${ROOT}/scripts/push-task.sh IndexUsers
+  ${ROOT}/scripts/push-task.sh IndexFeeds
+}
+
+print_help () {
+  echo "Usage: ${0} COMMAND"
+  echo ""
+  echo "Available commands:"
+  echo "index           Queues all indexing tasks"
+  echo "mappings        Create index with mappings"
+  echo "reset           Deletes and recreates index, queues tasks"
+  exit 1
+}
+
+if [ -z "${1}" ]; then
+  print_help
 fi
 
-if [ ${REINDEX} -eq 1 ]; then
-  docker exec ttio-dev-worker /data/code/Worker/push.php IndexUsers
-  docker exec ttio-dev-worker /data/code/Worker/push.php IndexFeeds
-fi
-
+case ${1} in
+  reset)
+    reset
+    ;;
+  mappings)
+    mappings
+    ;;
+  index)
+    index
+    ;;
+  *)
+    echo "Unknown command ${1}"
+esac
