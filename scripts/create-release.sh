c58fc352abbd8a854ed1b1639a23a9893b431979
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-01-06 11:51:17 +0100

    Update release script

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -6,9 +6,17 @@ if [ -z "${1}" ]; then
   exit 1
 fi
 
-CHANGELOG=$(git log --pretty=format:"- %s%n%b" --since="$(git show -s --format=%ad `git rev-list --tags --max-count=1`)" | grep -v "Merge branch" | grep .)
+CHANGELOG=$(git log --pretty=format:"- %s%n" --since="$(git show -s --format=%ad `git rev-list --tags --max-count=1`)" | grep -v "Merge branch" | grep .)
 MESSAGE="${1}\n\n${CHANGELOG}"
+FILE=$(mktemp)
 
-printf "${MESSAGE}\n"
+printf "${MESSAGE}" > ${FILE}
+${EDITOR} "${FILE}"
+
+MESSAGE=$(cat "${FILE}")
+
+printf "${MESSAGE}\n\n"
+
+read -p "Release? [ENTER] "
 
 printf "${MESSAGE}" | hub release create ${1} -f -
