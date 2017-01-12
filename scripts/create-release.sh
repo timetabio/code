da583362863d41a7973460e2afceb94cddbfdf9e
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2017-01-12 19:40:23 +0100

    Change time based changelog to diff between master and last tag

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -5,7 +5,7 @@ if [ -z "${1}" ]; then
   exit 1
 fi
 
-CHANGELOG=$(git log --pretty=format:"- %s%n" --since="$(git show -s --format=%ad `git rev-list --tags --max-count=1`)" | grep -v "Merge branch" | grep .)
+CHANGELOG=$(git log --pretty=format:"- %s%n" master...$(git describe --abbrev=0 --tags) | grep -v "Merge branch" | grep -v "Merge commit" | grep .)
 MESSAGE="${1}\n\n${CHANGELOG}"
 FILE=$(mktemp)
 
